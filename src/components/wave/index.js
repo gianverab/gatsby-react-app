a71afe78f4dba20729af1c2d0aa008ea56cb5503
Giancarlo Vera <gianverab@gmail.com>
2020-07-23 01:11:20 -0500

    styling static data

--- src/components/wave/index.js
+++ src/components/wave/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Wave = () => (
-  <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="100%" height="124" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill="white">
       <animate
         repeatCount="indefinite"
